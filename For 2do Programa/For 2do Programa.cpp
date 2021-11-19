798e022cf782578286d1aa209a33ba42d3e90c12
arash <arash@ARASHITORA>
2021-11-19 01:35:41 -0400

    Add project files.

---
+++ For 2do Programa/For 2do Programa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main() // Restara de 1 en 1 hasta el 1
+{
+    for (int i = 10; i > 0; i--)
+    {//Notemos que escribir i-- es similar a escribir i = i - 1
+        cout << i << endl;
+    }
+    return 0;
+}
+
